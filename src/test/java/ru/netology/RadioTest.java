011fc54d1a2c4201dc80e1c9442c75673dc036ca
vika_t <selga1991@yandex.ru>
2024-03-27 14:32:17 +0700

    full tests

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 public class RadioTest {
 
+
+    //Тесты на класс эквивалентности номера радиостанции
     @Test
     public void shouldSetCorrectStation() {
         Radio radio = new Radio();
@@ -35,6 +37,7 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    //Тесты граничных значений номера радиостанции
     @Test
     public void shouldSetNextStation() {
         Radio radio = new Radio();
@@ -83,8 +86,51 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    //Тесты классов эквивалентности громкости звука
     @Test
-    public void shouldIncreaseVolume() {
+    public void shouldSetCorrectVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(50);
+
+        int expected = 50;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetVolumeBelowMin() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(-50);
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetVolumeAfterMax() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(150);
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //Тесты граничных значений увеличения громкости звука
+    @Test
+    public void shouldVolumeUp() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(98);
+
+        radio.increaseVolume();
+
+        int expected = 99;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldVolumeUpToMax() {
         Radio radio = new Radio();
         radio.setCurrentVolume(99);
 
@@ -96,7 +142,7 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldSetMaxVolume() {
+    public void shouldSetAfterMaxVolume() {
         Radio radio = new Radio();
         radio.setCurrentVolume(100);
 
@@ -107,32 +153,9 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    //Тесты граничных значений уменьшения громкости звука
     @Test
-    public void shouldSetVolumeBelowZero() {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(0);
-
-        radio.increaseVolume();
-
-        int expected = 1;
-        int actual = radio.getCurrentVolume();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSetVolumeAboveMax() {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(101);
-
-        radio.increaseVolume();
-
-        int expected = 1;
-        int actual = radio.getCurrentVolume();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldDecreaseVolume() {
+    public void shouldVolumeDown() {
         Radio radio = new Radio();
         radio.setCurrentVolume(2);
 
@@ -142,9 +165,8 @@ public class RadioTest {
         int actual = radio.getCurrentVolume();
         Assertions.assertEquals(expected, actual);
     }
-
     @Test
-    public void shouldDecreaseVolumeToMin() {
+    public void shouldVolumeDownToMin() {
         Radio radio = new Radio();
         radio.setCurrentVolume(1);
 
@@ -154,9 +176,8 @@ public class RadioTest {
         int actual = radio.getCurrentVolume();
         Assertions.assertEquals(expected, actual);
     }
-
     @Test
-    public void shouldDecreaseVolumeBelowMin() {
+    public void shouldSetVolumeBelowZero() {
         Radio radio = new Radio();
         radio.setCurrentVolume(0);
 
